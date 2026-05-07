7785b7292946ad87a93c88436c7fb84782e6834d
ph3dbmw <ph3dz1@gmail.com>
2026-05-07 08:03:31 +0100

    Add Device Info and Network sensors

--- custom_components/router_tui/sensor.py
+++ custom_components/router_tui/sensor.py
@@ -27,7 +27,15 @@ async def async_setup_entry(hass, entry, async_add_entities):
         RouterWifi5DataReceivedSensor(coordinator),
         RouterExternalIPSensor(coordinator),
         RouterWifi24ClientsSensor(coordinator),
-        RouterWifi5ClientsSensor(coordinator)
+        RouterWifi5ClientsSensor(coordinator),
+        RouterSerialNumberSensor(coordinator),
+        RouterSoftwareVersionSensor(coordinator),
+        RouterUptimeSensor(coordinator),
+        RouterLocalIpSensor(coordinator),
+        RouterCpuLoadSensor(coordinator),
+        RouterMemoryUsageSensor(coordinator),
+        RouterPrimaryDnsSensor(coordinator),
+        RouterSecondaryDnsSensor(coordinator)
     ]
     
     async_add_entities(entities)
@@ -332,3 +340,144 @@ class RouterWifi5ClientsSensor(CoordinatorEntity, SensorEntity):
     @property
     def extra_state_attributes(self):
         return {"devices": self._get_clients()}
+
+class RouterSerialNumberSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Serial Number"
+        self._attr_unique_id = "router_serial_number"
+        self._attr_icon = "mdi:barcode"
+        self._attr_device_info = ROUTER_DEVICE_INFO
+
+    @property
+    def native_value(self):
+        open_data = self.coordinator.data.get("all_endpoints", {}).get("open")
+        if open_data and isinstance(open_data, list) and len(open_data) > 0 and isinstance(open_data[0], dict):
+            return open_data[0].get("serial_number", "Unknown")
+        return "Unknown"
+
+class RouterSoftwareVersionSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Software Version"
+        self._attr_unique_id = "router_software_version"
+        self._attr_icon = "mdi:update"
+        self._attr_device_info = ROUTER_DEVICE_INFO
+
+    @property
+    def native_value(self):
+        open_data = self.coordinator.data.get("all_endpoints", {}).get("open")
+        if open_data and isinstance(open_data, list) and len(open_data) > 0 and isinstance(open_data[0], dict):
+            return open_data[0].get("internal_firmware_version", open_data[0].get("firmware", "Unknown"))
+        return "Unknown"
+
+class RouterUptimeSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Uptime"
+        self._attr_unique_id = "router_uptime"
+        self._attr_icon = "mdi:clock-outline"
+        self._attr_device_info = ROUTER_DEVICE_INFO
+        self._attr_device_class = "duration"
+        self._attr_native_unit_of_measurement = "s"
+
+    @property
+    def native_value(self):
+        open_data = self.coordinator.data.get("all_endpoints", {}).get("open")
+        if open_data and isinstance(open_data, list) and len(open_data) > 0 and isinstance(open_data[0], dict):
+            try:
+                return int(open_data[0].get("uptime", 0))
+            except:
+                pass
+        return None
+
+class RouterLocalIpSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Local IPv4 Address"
+        self._attr_unique_id = "router_local_ipv4"
+        self._attr_icon = "mdi:ip-network"
+        self._attr_device_info = ROUTER_DEVICE_INFO
+
+    @property
+    def native_value(self):
+        open_data = self.coordinator.data.get("all_endpoints", {}).get("open")
+        if open_data and isinstance(open_data, list) and len(open_data) > 0 and isinstance(open_data[0], dict):
+            return open_data[0].get("gateway_ip", "Unknown")
+        return "Unknown"
+
+class RouterCpuLoadSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "CPU load (%)"
+        self._attr_unique_id = "router_cpu_load"
+        self._attr_icon = "mdi:cpu-64-bit"
+        self._attr_device_info = ROUTER_DEVICE_INFO
+
+    @property
+    def native_value(self):
+        return "Unknown"
+
+class RouterMemoryUsageSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Memory usage (%)"
+        self._attr_unique_id = "router_memory_usage"
+        self._attr_icon = "mdi:memory"
+        self._attr_device_info = ROUTER_DEVICE_INFO
+
+    @property
+    def native_value(self):
+        return "Unknown"
+
+class RouterPrimaryDnsSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Primary DNS Server"
+        self._attr_unique_id = "router_primary_dns"
+        self._attr_icon = "mdi:dns"
+        self._attr_device_info = ROUTER_DEVICE_INFO
+
+    @property
+    def native_value(self):
+        data = self.coordinator.data.get("all_endpoints", {}).get("dns_v4")
+        try:
+            if data and isinstance(data, list) and len(data) > 0 and isinstance(data[0], dict):
+                dns = data[0].get("DNS", {})
+                mode = dns.get("dnsMode", "DYNAMIC")
+                if mode == "DYNAMIC":
+                    dyn = dns.get("dynamic", [])
+                    s = dyn[0].get("server", "") if (dyn and isinstance(dyn, list) and isinstance(dyn[0], dict)) else ""
+                else:
+                    s = dns.get("static", {}).get("servers", "")
+                servers = [x.strip() for x in s.split(",") if x.strip()]
+                return servers[0] if len(servers) > 0 else "Unknown"
+        except:
+            pass
+        return "Unknown"
+
+class RouterSecondaryDnsSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Secondary DNS Server"
+        self._attr_unique_id = "router_secondary_dns"
+        self._attr_icon = "mdi:dns"
+        self._attr_device_info = ROUTER_DEVICE_INFO
+
+    @property
+    def native_value(self):
+        data = self.coordinator.data.get("all_endpoints", {}).get("dns_v4")
+        try:
+            if data and isinstance(data, list) and len(data) > 0 and isinstance(data[0], dict):
+                dns = data[0].get("DNS", {})
+                mode = dns.get("dnsMode", "DYNAMIC")
+                if mode == "DYNAMIC":
+                    dyn = dns.get("dynamic", [])
+                    s = dyn[0].get("server", "") if (dyn and isinstance(dyn, list) and isinstance(dyn[0], dict)) else ""
+                else:
+                    s = dns.get("static", {}).get("servers", "")
+                servers = [x.strip() for x in s.split(",") if x.strip()]
+                return servers[1] if len(servers) > 1 else "Unknown"
+        except:
+            pass
+        return "Unknown"
